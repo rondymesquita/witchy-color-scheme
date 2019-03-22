69fd5f36fb5ba593b447eee066afc2b99419e6db
rondymesquita <rondinellimesquitas@gmail.com>
2019-03-22 14:22:34 -0300

    init

---
+++ colors.js
@@ -0,0 +1,36 @@
+module.exports.colors = {
+
+  // A color set can have both light and dark variants, but is only required
+  // to have one.
+  dark: {
+
+    // Colors can be defined in any valid CSS format.
+
+    // accent0-7 should be the main accent colors of your theme. See the table
+    // in the "Color mappings" section for how the colors will be used in your
+    // new themes.
+    accent7: '#9c2930',
+    accent1: '#a56045',
+    accent2: '#de8256',
+    accent3: '#d53739',
+    accent0: '#67232b',
+    accent5: '#757d87',
+    accent6: '#e9b99a',
+    accent4: '#593c34',
+    accent8: '#678C9A',
+    accent9: '#4E7483',
+
+    // shade0-7 should be shades of the same hue, with shade0 being the
+    // background and shade7 being the foreground. If you omit the
+    // intermediate shades (1 through 6), they will be calculated automatically
+    // for you.
+    shade0: '#1d1f2b',
+    shade7: '#FFFCFF'
+
+  },
+
+  // Same as above, except that shade0 should be the lightest and shade7 should
+  // be the darkest.
+  // light: { ... },
+
+};
